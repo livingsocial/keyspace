7bed043434885dab920cb090e4a82462debed1dd
Tony Arcieri <tony.arcieri@gmail.com>
2012-04-30 23:36:11 -0700

    Encryption and decryption support

--- lib/vault/bucket.rb
+++ lib/vault/bucket.rb
@@ -5,9 +5,10 @@ module Vault
   class Bucket
     attr_reader :id, :capabilities, :path
 
-    class InvalidCapabilityError < ArgumentError; end
+    class InvalidCapabilityError < ArgumentError; end # potentially forged credentials
+    class InvalidSignatureError < ArgumentError; end  # potentially forged data
 
-    # Generate a new bucket
+    # Generate a completely new bucket
     def self.create
       signing_key = OpenSSL::PKey::DSA.new(2048)
 
@@ -19,6 +20,7 @@ module Vault
       end
     end
 
+    # Instantiate a bucket from its constituent keys
     def initialize(verify_key, read_key = nil, signing_key = nil)
       if signing_key
         if read_key && read_key != Digest::SHA256.hexdigest(signing_key)
@@ -28,7 +30,7 @@ module Vault
         @signing_key = OpenSSL::PKey::DSA.new(signing_key)
 
         if @signing_key.public_key.to_der != verify_key
-          raise InvalidCapabilityError, "verify key does not match signing key"
+          raise InvalidCapabilityError, "potentially forged credentials: verify key does not match signing key"
         end
       else
         @signing_key = nil
@@ -46,12 +48,65 @@ module Vault
       @capabilities << :verify if @verify_key
     end
 
+    # Encrypt a key/value pair for insertion into the vault
+    # Key is not a cryptographic key, but a human meaningful name that this
+    # data should be associated with
+    def encrypt(key, value, timestamp = Time.now)
+      raise ArgumentError, "key too long" if key.size > 0xFF
+
+      cipher = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
+      cipher.encrypt
+
+      # TODO: KDF w\ a random salt here perhaps?
+      cipher.key = @read_key
+      cipher.iv  = iv = cipher.random_iv
+
+      ciphertext =  cipher.update(value)
+      ciphertext << cipher.final
+
+      # TODO: hash/encrypt key
+      # TODO: Y2038 compliance o_O
+      message   = [key.size, key, timestamp.utc.to_i, iv, ciphertext.size, ciphertext].pack("CA*NA16NA*")
+      signature = @signing_key.syssign Digest::SHA1.digest(message)
+      [signature.size, signature, message].pack("CA*A*")
+    end
+
+    # Decrypt an encrypted value, checking its authenticity with the bucket's verify key
+    def decrypt(encrypted_value)
+      signature_size, rest = encrypted_value.unpack("CA*")
+      signature, message = rest.unpack("A#{signature_size}A*")
+      digest = Digest::SHA1.digest(message)
+
+      unless @verify_key.sysverify(digest, signature)
+        raise InvalidSignatureError, "potentially forged data: signature mismatch"
+      end
+
+      key_size, rest = message.unpack("CA*")
+      key, timestamp, iv, message_size, ciphertext = rest.unpack("A#{key_size}NA16NA*")
+
+      # Cast to Time from an integer timestamp
+      timestamp = Time.at(timestamp)
+
+      cipher = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
+      cipher.decrypt
+
+      cipher.key = @read_key
+      cipher.iv  = iv
+
+      plaintext = cipher.update(ciphertext)
+      plaintext << cipher.final
+
+      [key, plaintext, timestamp]
+    end
+
     def inspect
       "#<#{self.class} #{@id} [#{@capabilities.join(' ')}]}>"
     end
 
+    # Save a newly created bucket to disk
     def save
       path.mkdir
+      path.chmod 0700
       path.join('verify.key').open('w', 0600) { |f| f << @verify_key.to_der }
     end
 
--- spec/vault/bucket_spec.rb
+++ spec/vault/bucket_spec.rb
@@ -1,12 +1,27 @@
 require 'spec_helper'
 
 describe Vault::Bucket do
+  let(:example_key)   { 'foobar' }
+  let(:example_value) { 'X' * 5000 }
+  let(:example_date)  { Time.now }
+
   it "creates buckets" do
     with_bucket do |bucket|
       bucket.should be_a Vault::Bucket
     end
   end
-  
+
+  it "encrypts and decrypts values for storage in a bucket" do
+    with_bucket do |bucket|
+      encrypted_value = bucket.encrypt(example_key, example_value, example_date)
+      key, value, date = bucket.decrypt(encrypted_value)
+
+      key.should   eq example_key
+      value.should eq example_value
+      date.utc.to_i.should eq example_date.utc.to_i
+    end
+  end
+
   def with_bucket(&block)
     bucket = Vault::Bucket.create
     begin
