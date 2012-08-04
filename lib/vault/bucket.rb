be31c9e77eb53f31c29eb176e8ce435b50a36f46
Tony Arcieri <tony.arcieri@gmail.com>
2012-08-03 18:00:16 -0700

    Factor all crypto into Vault::Capability

--- lib/vault/bucket.rb
+++ lib/vault/bucket.rb
@@ -8,7 +8,7 @@ module Vault
   class Bucket
     extend Forwardable
     def_delegators :@capability, :id, :signature_key, :encryption_key, :capabilities
-
+    def_delegators :@capability, :encrypt, :verify, :decrypt, :public_key
 
     # Generate a completely new bucket
     def self.create(id)
@@ -18,66 +18,6 @@ module Vault
     # Load a bucket from a capability stringd
     def initialize(capability_string)
       @capability = Capability.parse(capability_string)
-      @signer = SignatureAlgorithm.new(@capability.signature_key)
-    end
-
-    # Obtain the public key for this bucket
-    def public_key
-      @signer.public_key
-    end
-
-    # Encrypt a key/value pair for insertion into the vault
-    # Key is not a cryptographic key, but a human meaningful id that this
-    # data should be associated with
-    def encrypt(key, value, timestamp = Time.now)
-      raise InvalidCapabilityError, "don't have write capability" unless @signer.private_key?
-      raise ArgumentError, "key too long" if key.size > 0xFF
-
-      cipher = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
-      cipher.encrypt
-
-      cipher.key = encryption_key
-      cipher.iv  = iv = cipher.random_iv
-
-      ciphertext =  cipher.update(value)
-      ciphertext << cipher.final
-
-      # TODO: hash/encrypt key
-      message   = [key.size, key, timestamp.utc.to_i, iv, ciphertext.size, ciphertext].pack("CA*QA16NA*")
-      signature = @signer.sign(message)
-      [signature.size, signature, message].pack("Ca*a*")
-    end
-
-    # Determine if the given encrypted value is authentic
-    def verify(encrypted_value)
-      signature_size, rest = encrypted_value.unpack("Ca*")
-      signature, message = rest.unpack("A#{signature_size}A*")
-
-      @signer.verify(message, signature)
-    end
-
-    # Decrypt an encrypted value, checking its authenticity with the bucket's verify key
-    def decrypt(encrypted_value)
-      raise InvalidCapabilityError, "don't have read capability for this bucket" unless encryption_key
-      raise InvalidSignatureError, "potentially forged data: signature mismatch" unless verify(encrypted_value)
-
-      signature_size, rest = encrypted_value.unpack("CA*")
-      signature, key_size, rest = rest.unpack("a#{signature_size}Ca*")
-      key, timestamp, iv, message_size, ciphertext = rest.unpack("a#{key_size}Qa16Na*")
-
-      # Cast to Time from an integer timestamp
-      timestamp = Time.at(timestamp)
-
-      cipher = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
-      cipher.decrypt
-
-      cipher.key = encryption_key
-      cipher.iv  = iv
-
-      plaintext = cipher.update(ciphertext)
-      plaintext << cipher.final
-
-      [key, plaintext, timestamp]
     end
 
     def inspect
--- lib/vault/capability.rb
+++ lib/vault/capability.rb
@@ -24,9 +24,69 @@ module Vault
 
     def initialize(id, signature_key, encryption_key = nil)
       @id, @signature_key, @encryption_key = id, signature_key, encryption_key
+      @signer = SignatureAlgorithm.new(signature_key)
 
       @capabilities = 'r'
       @capabilities << 'w' if SignatureAlgorithm.new(signature_key).private_key?
+    end
+
+    # TODO: replace this with a proper capability degrading method
+    def public_key
+      @signer.public_key
+    end
+
+    # Encrypt a key/value pair for insertion into the vault
+    # Key is not a cryptographic key, but a human meaningful id that this
+    # data should be associated with
+    def encrypt(key, value, timestamp = Time.now)
+      raise InvalidCapabilityError, "don't have write capability" unless @signer.private_key?
+      raise ArgumentError, "key too long" if key.size > 0xFF
+
+      cipher = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
+      cipher.encrypt
+
+      cipher.key = encryption_key
+      cipher.iv  = iv = cipher.random_iv
+
+      ciphertext =  cipher.update(value)
+      ciphertext << cipher.final
+
+      # TODO: hash/encrypt key
+      message   = [key.size, key, timestamp.utc.to_i, iv, ciphertext.size, ciphertext].pack("CA*QA16NA*")
+      signature = @signer.sign(message)
+      [signature.size, signature, message].pack("Ca*a*")
+    end
+
+    # Determine if the given encrypted value is authentic
+    def verify(encrypted_value)
+      signature_size, rest = encrypted_value.unpack("Ca*")
+      signature, message = rest.unpack("A#{signature_size}A*")
+
+      @signer.verify(message, signature)
+    end
+
+    # Decrypt an encrypted value, checking its authenticity with the bucket's verify key
+    def decrypt(encrypted_value)
+      raise InvalidCapabilityError, "don't have read capability for this bucket" unless encryption_key
+      raise InvalidSignatureError, "potentially forged data: signature mismatch" unless verify(encrypted_value)
+
+      signature_size, rest = encrypted_value.unpack("CA*")
+      signature, key_size, rest = rest.unpack("a#{signature_size}Ca*")
+      key, timestamp, iv, message_size, ciphertext = rest.unpack("a#{key_size}Qa16Na*")
+
+      # Cast to Time from an integer timestamp
+      timestamp = Time.at(timestamp)
+
+      cipher = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
+      cipher.decrypt
+
+      cipher.key = encryption_key
+      cipher.iv  = iv
+
+      plaintext = cipher.update(ciphertext)
+      plaintext << cipher.final
+
+      [key, plaintext, timestamp]
     end
 
     def to_s
