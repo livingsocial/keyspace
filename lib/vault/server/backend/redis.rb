820850c5142da8fb3c4f09029241f34134224542
Tony Arcieri <tony.arcieri@gmail.com>
2012-08-17 17:19:51 -0700

    Persist values in Redis

--- lib/vault/server/backend/redis.rb
+++ lib/vault/server/backend/redis.rb
@@ -16,7 +16,28 @@ module Vault
           verifycap = Capability.parse(verifycap) if verifycap.is_a? String
 
           # TODO: check if bucket already exists
-          @redis.set(verifycap.id, verifycap)
+          @redis.set("verifycap::#{verifycap.id}", verifycap)
+        end
+
+        # Retrieve a value from the bucket
+        # No verification of authenticity is performed as it should be
+        # performed by Vault::Server prior to storage
+        def get(bucket, key)
+          @redis.get(key_name(bucket, key))
+        end
+
+        # Put a value in a bucket
+        # No verification of authenticity is performed as it should be
+        # performed by Vault::Server prior to storage
+        def set(bucket, key, value)
+          @redis.set(key_name(bucket, key), value)
+        end
+
+      private
+
+        # Obtain the key name for a particular bucket/key pair
+        def key_name(bucket, key)
+          "value::#{bucket}:#{key}"
         end
       end
     end
--- spec/vault/server/backend/redis_spec.rb
+++ spec/vault/server/backend/redis_spec.rb
@@ -2,7 +2,19 @@ require 'spec_helper'
 
 describe Vault::Server::Backend::Redis do
   subject { Vault::Server::Backend::Redis.new(::Redis.new(:db => 10))}
+
+  let(:example_bucket)    { :foobar }
+  let(:example_key)       { :baz }
+  let(:example_value)     { 'quux' }
+  let(:example_verifycap) { Vault::Capability.generate(example_bucket).degrade(:verify) }
+
   it "creates buckets from verifycaps" do
-    subject.create Vault::Capability.generate(:foobar).degrade(:verify)
+    subject.create example_verifycap
+  end
+
+  it "stores data in buckets" do
+    subject.create example_verifycap
+    subject.set(example_bucket, example_key, example_value)
+    subject.get(example_bucket, example_key).should == example_value
   end
 end
